2f2d2ef7699e372729a4209692d543b148cc6d45
Grtchn <gretchenopie@gmail.com>
2019-06-19 15:56:55 -0400

    added some more components

---
+++ src/Components/Location.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+
+class LocationForm extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          
+        </form>
+      </div>
+    )
+  }
+
+}
+
+export default LocationForm
---
+++ src/Components/NoTornado.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoTornado = () => {
+  return (
+    <h1>No Tornado...</h1>
+  )
+  }
+
+export default NoTornado
